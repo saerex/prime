c0ecd17fcf180d7ebc962804be793e94d1e39256
saerex <yu.orlov2012@yandex.ua>
2016-11-28 21:44:41 +0200

    new test php

--- js/modal-init.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    var $content = $('share-options').detach();
-    $('#click-image').on('click', function(e){
-      var path = $(this).attr('src');
-      console.log(path);
-        e.preventDefault();
-        modal.open({content: path, width:340, height: 300});
-    });
-}());
