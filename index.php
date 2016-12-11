606c64b52826eec896e7fa9a89c0fc8cd477d55d
saerex <yu.orlov2012@yandex.ua>
2016-12-11 21:53:22 +0200

    try php file

---
+++ index.php
@@ -0,0 +1,16 @@
+﻿<html>
+<head>
+<meta charset="utf-8">
+<link type="text/css" rel="stylesheet" href="bookmain.css">
+<title>Подземелья черного замка</title>
+<script type="text/javascript">
+
+</script>
+</head>
+<body>
+<div id="main">
+<p><img style="height:90%;" src="images/first.png" alt="Подземелья темного замка" id="firstimg"/></p>
+<a href="scene.html">Начать путешествие</a>
+</div>
+</body>
+</html>
